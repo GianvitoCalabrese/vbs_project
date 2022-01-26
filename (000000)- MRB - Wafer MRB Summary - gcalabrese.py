401b58e680eed2cfaf428cdf04687666e6856629
Gianvito Calabrese <gcalabrese296@gmail.com>
2022-01-26 20:52:06 +0100

    Job done

--- (000000)- MRB - Wafer MRB Summary - gcalabrese.py
+++ (000000)- MRB - Wafer MRB Summary - gcalabrese.py
@@ -4,10 +4,10 @@ import xlwings as xw
 def main():
     wb = xw.Book.caller()
     sheet = wb.sheets[0]
-    if sheet["A1"].value == "Hello xlwings!":
-        sheet["A1"].value = "Bye xlwings!"
+    if sheet.range("A1").value == "Hello xlwings!":
+        sheet.range("A1").value = "Bye xlwings!"
     else:
-        sheet["A1"].value = "Hello xlwings!"
+        sheet.range("A1").value = "Hello xlwings!"
 
 
 @xw.func
